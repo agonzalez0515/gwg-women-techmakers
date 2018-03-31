257ecd2ab3bb418364a8fbd808582ecfff3b8b2d
Angelica Gonzalez <agonzalez0515@gmail.com>
2018-03-31 12:59:10 -0700

    move sw to root and add install function again

--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,34 @@
 CACHE_NAME = "mappa-v1"
 
+const urlsToCache =[
+  '/',
+  '/static/js/main.e3f515ae.js',
+  '/static/css/main.c67ba17a.css',
+  '/index.html'
+];
+
+
+
+// The first time the user starts up the app, 'install' is triggered.
+self.addEventListener('install', function(event) {
+  console.log("installing")
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(function(cache) {
+          cache.addAll(urlsToCache)
+        })
+    );
+});
+
+// When the webpage goes to fetch files, we intercept that request and serve up the matching files
+// if we have them
+self.addEventListener('fetch', function(event) {
+      event.respondWith(
+          caches.match(event.request).then(function(response) {
+              return response || fetch(event.request);
+          })
+      );
+});
 
 //This is caching the static map on the first load, Called "Cache then network" recipe
 self.addEventListener('fetch', function (event) {
