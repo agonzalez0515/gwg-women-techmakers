7fee79b1471853e00bbf93cfe2b6a2f6ac7755d1
Angelica Gonzalez <agonzalez0515@gmail.com>
2018-03-31 10:39:55 -0700

    fix typo

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -19,8 +19,8 @@ const isLocalhost = Boolean(
   );
   
   export default function register() {
-    // 
-    if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+
+    if ('serviceWorker' in navigator) {
       // The URL constructor is available in all browsers that support SW.
       const publicUrl = new URL(process.env.PUBLIC_URL, window.location);
       if (publicUrl.origin !== window.location.origin) {
@@ -31,7 +31,7 @@ const isLocalhost = Boolean(
       }
   
       window.addEventListener('load', () => {
-        const swUrl = `https://mappa-gwg.herokuapp.com/service-worker.js`;
+        const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
   
         if (isLocalhost) {
           // This is running on localhost. Lets check if a service worker still exists or not.
